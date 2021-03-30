f35a9025dc964ca11dc7097419f4d239a3d1ab0b
Ramin <rasadeghnasab@gmail.com>
2021-03-31 01:01:54 +0430

    - Basice migration tables added

---
+++ project/database/migrations/2021_03_30_152127_create_opening_hours_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOpeningHoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('opening_hours', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('timeable');
+            $table->time('from');
+            $table->time('to');
+            $table->enum('day', [
+                'monday',
+                'tuesday',
+                'wednesday',
+                'thursday',
+                'friday',
+                'saturday',
+                'sunday',
+            ]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('opening_hours');
+    }
+}
