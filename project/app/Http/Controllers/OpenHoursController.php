6c3d689fd67f234be7cdc1b0739b49c9bfaee381
Ramin <rasadeghnasab@gmail.com>
2021-04-01 18:26:27 +0430

    - Change opening hours to openhours

---
+++ project/app/Http/Controllers/OpenHoursController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OpenHour;
+use Illuminate\Http\Request;
+
+class OpenHoursController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OpenHour  $openHour
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OpenHour $openHour)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OpenHour  $openHour
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OpenHour $openHour)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OpenHour  $openHour
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OpenHour $openHour)
+    {
+        //
+    }
+}
